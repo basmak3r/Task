6df49c3dc2ac94cd7eed46a34d09303cc45c45cb
basmak3r <bas.mak3r@gmail.com>
2022-10-27 13:05:08 +0530

    Process

--- Andriod/app/src/main/java/com/example/batterydata/UserDao.kt
+++ Andriod/app/src/main/java/com/example/batterydata/UserDao.kt
@@ -1,14 +1,19 @@
 package com.example.batterydata
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
+
 @Dao
 interface UserDao {
     @Query("SELECT * FROM BatteryData")
     fun getAll(): List<User>
+
+    @Query("SELECT * FROM BatteryData WHERE timestamp BETWEEN :dateStart AND :dateEnd")
+    fun getDateResult(dateStart : String, dateEnd :String):List<User>
 
 
     @Insert
